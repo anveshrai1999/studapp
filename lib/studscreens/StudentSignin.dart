955f13b7d122545df8bd67af55d002b3129372ba
Anveshrai <anveesh.rai@gmail.com>
2020-02-02 11:20:48 +0530

    <Message for the commit>

--- lib/studscreens/StudentSignin.dart
+++ lib/studscreens/StudentSignin.dart
@@ -14,36 +14,96 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(),
-      body: Form(
-          key: _formKey,
-          child: Column(
+      backgroundColor: Colors.white,
+      body: ListView(children: <Widget>[
+        Container(
+          height: 400,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage('images/background.png'))),
+          child: Stack(
             children: <Widget>[
-              TextFormField(
-                validator: (input) {
-                  if (input.isEmpty) {
-                    return 'Provide an email';
-                  }
-                },
-                decoration: InputDecoration(labelText: 'Email'),
-                onSaved: (input) => _email = input,
-              ),
-              TextFormField(
-                validator: (input) {
-                  if (input.length < 6) {
-                    return 'Longer password please';
-                  }
-                },
-                decoration: InputDecoration(labelText: 'Password'),
-                onSaved: (input) => _password = input,
-                obscureText: true,
-              ),
-              RaisedButton(
-                onPressed: signIn,
-                child: Text('Sign in'),
-              ),
+              Positioned(
+                  left: 30,
+                  width: 80,
+                  height: 250,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/light-1.png'))),
+                  )),
+              Positioned(
+                  left: 140,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/light-2.png'))),
+                  )),
+              Positioned(
+                  right: 40,
+                  top: 40,
+                  width: 80,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/clock.png'))),
+                  ))
             ],
-          )),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  validator: (input) {
+                    if (input.isEmpty) {
+                      return 'Provide an email';
+                    }
+                    if (!RegExp(
+                            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                        .hasMatch(input)) {
+                      return 'please enter a valid email id';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'EMAIL', icon: Icon(Icons.email)),
+                  onSaved: (input) => _email = input,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (input) {
+                    if (input.length < 6) {
+                      return 'Longer password please';
+                    }
+                  },
+                  decoration: InputDecoration(labelText: 'PASSWORD',icon: Icon(Icons.input),suffixIcon: Icon(Icons.remove_red_eye,
+                  )),
+                  onSaved: (input) => _password = input,
+                  obscureText: true,
+                ),
+                RaisedButton(
+                  elevation: 5,
+
+                  color: Colors.white,
+                  onPressed: signIn,
+                  child: Text('Sign in',
+                  style: TextStyle(
+                    color: Colors.deepPurpleAccent,
+
+                  ),),
+                ),
+              ],
+            )),
+      ]),
     );
   }
 
